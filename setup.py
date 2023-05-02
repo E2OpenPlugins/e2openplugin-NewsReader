7334ea27efad8978bc97d0e9a98ed2604ca8d823
Mr.Servo <mrservo@gmx.de>
2023-05-02 10:16:02 +0200

    [Newsreader] v1.0 update
    The plugin was completely revised:
    - code was adapted to Python 3
    - main translation file and German translation added
    - skin design was enlarged to UHD/HD
    - new plugin logo created (special thanks to stein17 @ openA.TV)
    - provided sample feeds.xml have been updated

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 pkg = 'Extensions.NewsReader'
 setup(name='enigma2-plugin-extensions-newsreader',
-       version='0.1',
-       description='RSS newsreader',
+       version='1.0',
+       description='NewsReader',
        package_dir={pkg: 'plugin'},
        packages=[pkg],
        data_files=[('/etc',
