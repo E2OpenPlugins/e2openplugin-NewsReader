e8e2c15564810f5c1b40b4c55e8d10c863b86a6b
Mr.Servo <mrservo@gmx.de>
2023-05-04 09:55:26 +0200

    [Newsreader] v1.0 setup.py fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
+import setup_translate
 
 pkg = 'Extensions.NewsReader'
 setup(name='enigma2-plugin-extensions-newsreader',
        version='1.0',
-       description='NewsReader',
-       package_dir={pkg: 'plugin'},
+       description='NewsReader for reading RSS-feeds',
        packages=[pkg],
-       data_files=[('/etc',
-           ['plugin/data/feeds.xml'])]
-       )
+       package_dir={pkg: 'plugin'},
+       package_data={pkg: ['*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
+       data_files=[('/etc', ['plugin/data/feeds.xml'])]
+       cmdclass=setup_translate.cmdclass,  # for translation
+      )
